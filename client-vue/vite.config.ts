6ce31c4f719a33992b6a06cd28a0d2812cb8ddd8
15233281594 <15233281594@users.noreply.github.com>
2026-06-11 00:41:52 +0800

    chore: rename client project

--- client-vue/vite.config.ts
+++ client-vue/vite.config.ts
@@ -5,7 +5,19 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '/',
   plugins: [vue()],
+  server: {
+    host: 'localhost',
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:9999',
+        changeOrigin: false,
+      },
+    },
+  },
   preview: {
+    host: 'localhost',
+    port: 4173,
     allowedHosts: true,
   },
 })
